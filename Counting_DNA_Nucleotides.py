eb6c4a349628ac9b309c7a46602a352f59d0c4c8
Ninaad Kalla <ninaadkalla@gmail.com>
2023-07-10 11:15:06 -0400

    Implemented new solution using dict. Added example.

--- Counting_DNA_Nucleotides.py
+++ Counting_DNA_Nucleotides.py
@@ -1,18 +1,31 @@
+# First solution
 def count_nucleotides(seq:list):
 
-    A, C, G, T = 0, 0, 0, 0
-    new_list = list(seq)
+    nuc_dict = {"A":0, "C":0, "G":0, "T":0}
 
-    for x in new_list:
-        if x == 'A':
-            A += 1
-        elif x == 'C':
-            C += 1
-        elif x == 'G':
-            G += 1
-        elif x == 'T':
-            T += 1
+    for nuc in seq:
+        if nuc == "A" or nuc == "C" or nuc =='G' or nuc == "T":
+            nuc_dict[nuc] += 1
 
-    return A, C, G, T
+    return nuc_dict["A"], nuc_dict["C"], nuc_dict["G"], nuc_dict["T"]
 
-print(count_nucleotides('ACGTCGTAGTATGGGTACCC'))
+print(count_nucleotides('AGACGTCTCGTTCTAGAAGGTCGACGCCACAAGCTCTAATGCAGCGGTGAT\
+                        CATTGCGCCTCTGTGTTAACCTCTGCGACCCCTCGGTTCGTGACGCGATCCT\
+                        TAGTTCAATCTATACCCTCATGCCTCTGGACCTGATTCGTCAGATTTTCCTC\
+                        TGACGAAGAAACAGCCACCCAGCCTTGCTCTTCCGACCACTAATCGCTATCG\
+                        CTTTCTGGGCAGTGCCGCTAAACGCTTTGTGACGATACGTGGATCCAACCTG\
+                        GACCCAGTCGCTACCGACAGCGTTCGCAAATGATGGGTCCAAGGAAGCATAG\
+                        TTAGGAAGCCGGGACACGGATGGAGTAACTATGAACGCAGAACCATCGCGCA\
+                        TTGGCGTGCGACTGTCTGGAGAGAACATAGGACATGGACCTGTACGTTAATC\
+                        CGAATTGTGCGGGCACCCGGACCCTCAGGTACCTAGGCTTGGACTACCTAGA\
+                        GTATGGGGAACTGTAGTGAAGGCTCAGGCTCATAATAATTTTACGCTGCTTT\
+                        GTACTCCTGAAAAGCTGAAGCTAATATATTGACTTATAGTCTTTAAGAAGAC\
+                        GTGTAGGAATCACGGCGGACCTCGTCCTGGGTAAGGTAAGAAAATTTGAGTA\
+                        AAGAAGCGATTACACCCCGAGCGCTCCTATCCCCCGTCACGTGGCTAAACTG\
+                        GTCCTGCACACTTTTCAGTACCAGCCACTTGGGTTCGCTAATCGGGAGATCT\
+                        CGACCGAACTTTTTAACCTAGTACCACGCCAACGATAACCGGCAATGCAATT\
+                        TGTCATATCACGGCAAGGAGCGATGTTCGGATACTACCGTAGTTAAGGTTAT\
+                        TGAACCTCTGCTGCTATCAGGGAGAGGAGTGATCCCACTTTTTGTCGTAGCT\
+                        CTATGATAGGATAGGTTGG'))
+
+# returns (225, 229, 224, 224)
