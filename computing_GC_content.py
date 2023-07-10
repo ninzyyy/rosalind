30f196d5691a19a9a1deccbf134dbd7cb7086d5e
Ninaad Kalla <ninaadkalla@gmail.com>
2023-07-10 18:41:27 -0400

    Implemented best solution for challenge using FASTA format file

--- computing_GC_content.py
+++ computing_GC_content.py
@@ -1,12 +1,32 @@
 
-def computeGC(seq:str):
 
-    GC = seq.count("G") + seq.count("C")
-    total = len(seq)
+f = open('rosalind_gc.txt', 'r') #reads FASTA file
 
-    return (GC/total)*100
+max_gc_name, max_gc_content = '', 0
 
+buf = f.readline().rstrip()
+while buf:
+    seq_name, seq = buf[1:], ""
+    buf = f.readline().rstrip()
+    while not buf.startswith('>') and buf:
+        seq = seq + buf
+        buf = f.readline().rstrip()
+    seq_gc_content =((seq.count("C") + seq.count("G")) / len(seq))
+    if seq_gc_content > max_gc_content:
+        max_gc_name, max_gc_content = seq_name, seq_gc_content
 
+print(max_gc_name, max_gc_content*100)
+f.close()
 
-print(computeGC('CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGACTGGGAACCTGCGGGCAGTAGGTGGAAT'))
-# returns 60.91954
+# Example of a FASTA format file
+'''
+>Rosalind_6404
+CCTGCGGAAGATCGGCACTAGAATAGCCAGAACCGTTTCTCTGAGGCTTCCGGCCTTCCC
+TCCCACTAATAATTCTGAGG
+>Rosalind_5959
+CCATCGGTAGCGCATCCTTAGTCCAATTAAGTCCCTATCCAGGCGCTCCGCCGAAGGTCT
+ATATCCATTTGTCAGCAGACACGC
+>Rosalind_0808
+CCACCCTCGTGGTATGGCTAGGCATTCAGGAACCGGAGAACGCTTCAGACCAGCCCGGAC
+TGGGAACCTGCGGGCAGTAGGTGGAAT
+'''
