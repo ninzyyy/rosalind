46f079f62cf8927267045909bf35c94452df93a3
Ninaad Kalla <ninaadkalla@gmail.com>
2023-07-18 13:26:13 -0400

    Challenge completed successfully.

--- 11_mortal_fibonacci_rabbits.py
+++ 11_mortal_fibonacci_rabbits.py
@@ -1,6 +1,14 @@
 
 def mortalRabbits(months, lifespan):
 
-    generations = [1] + [0] * lifespan
+    generations = [1] + [0]*(lifespan - 1) #[1, 0, ... , lifespan - 1]
 
-print(mortalRabbits(6,3))
+    for i in range(months-1): #every month except initial
+
+        newborn_rabbits = sum(generations[1:]) #every gen > 1
+
+        generations = [newborn_rabbits] + generations[:-1]
+
+    return sum(generations)
+
+print(mortalRabbits(100,20))
